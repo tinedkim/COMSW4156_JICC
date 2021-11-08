dff184d114515cbe8caeb37e5c12eb59376d24e7
Catelen Wu <catelenwu@gmail.com>
2021-11-07 23:09:51 -0500

    Check dining hall status to see which are open

---
+++ Src/main_dining.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+from scrapingant_client import ScrapingAntClient
+import requests
+
+
+dining_url = 'https://dining.columbia.edu/'
+r = requests.get(dining_url)
+
+client = ScrapingAntClient(token='8a2062388f4f4cba84269e04c380cd10')
+
+content= client.general_request(dining_url).content
+soup = BeautifulSoup(content, features='html5lib')
+open_halls = []
+
+dining_halls = soup.find('div', attrs = {'class': 'col-xs-12 col-md-6'})
+for hall in dining_halls.findAll('div', attrs = {'class': 'location'}):
+    spans = hall.findAll('span')
+    hall_name = spans[0].text
+    hall_status = spans[1].text
+    if hall_status == 'Open':
+        open_halls.append(hall_name)
