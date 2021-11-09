697b34101e91bf4d72aa7f4fb4305be283b9149a
Christine Kim <30416401+tinedkim@users.noreply.github.com>
2021-11-09 12:32:13 -0500

    Add data scraping for food items (#1)
    * Check dining hall status to see which are open
    
    * Delete unused line of code
    
    * Scrape food items from each dining hall
    
    * Add function for scraping
    
    Co-authored-by: Catelen Wu <catelenwu@gmail.com>

---
+++ Src/main_dining.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+from scrapingant_client import ScrapingAntClient
+from dotenv import load_dotenv
+import os
+import requests
+
+dining_url = 'https://dining.columbia.edu/'
+
+load_dotenv()
+token = os.environ.get("scraping-token")
+client = ScrapingAntClient(token=token)
+
+content= client.general_request(dining_url).content
+soup = BeautifulSoup(content, features='html5lib')
+open_halls = []
+
+dining_halls = soup.find('div', attrs = {'class': 'col-xs-12 col-md-6'})
+for hall in dining_halls.findAll('div', attrs = {'class': 'location'}):
+    spans = hall.findAll('span')
+    hall_name = spans[0].text
+    hall_status = spans[1].text
+    if hall_status == 'Open':
+        open_halls.append(hall_name)
