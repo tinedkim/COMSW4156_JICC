2809529ec2390eb75bf5f0a1f9d3f175f10b6e17
Christine Kim <30416401+tinedkim@users.noreply.github.com>
2021-11-10 20:13:30 -0500

    Implement database connection and populate food items (#6)

--- Src/database.py
+++ Src/database.py
@@ -1,4 +1,22 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+from sqlalchemy import *
+from menu_items import scrape_all
 
-#testing
-conn = psycopg2.connect(database='postgres')
+load_dotenv()
+
+DATABASEURI = os.environ.get("database-uri")
+
+engine = create_engine(DATABASEURI, connect_args={'connect_timeout': 10})
+
+
+def populate_menu_items():
+    menus = scrape_all()
+    with engine.connect() as connection:
+        for i in range(1, 4):
+            dining_hall = menus[i]
+            for key, val in dining_hall.items():
+                connection.execute("INSERT INTO foodItem(foodName, imageURL, \
+                                   diningHall) VALUES (%s, %s, %d)",
+                                   key, val, dining_hall)
