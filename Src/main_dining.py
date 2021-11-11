53bd319d47690011c67d0da234c2ef2d130e2d1d
Catelen Wu <catelenwu@gmail.com>
2021-11-10 19:08:17 -0500

    Fix code style

--- Src/main_dining.py
+++ Src/main_dining.py
@@ -10,12 +10,12 @@ load_dotenv()
 token = os.environ.get("scraping-token")
 client = ScrapingAntClient(token=token)
 
-content= client.general_request(dining_url).content
+content = client.general_request(dining_url).content
 soup = BeautifulSoup(content, features='html5lib')
 open_halls = []
 
-dining_halls = soup.find('div', attrs = {'class': 'col-xs-12 col-md-6'})
-for hall in dining_halls.findAll('div', attrs = {'class': 'location'}):
+dining_halls = soup.find('div', attrs={'class': 'col-xs-12 col-md-6'})
+for hall in dining_halls.findAll('div', attrs={'class': 'location'}):
     spans = hall.findAll('span')
     hall_name = spans[0].text
     hall_status = spans[1].text
