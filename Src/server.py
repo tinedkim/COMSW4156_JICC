c4adc11898be260af379e56e0c1db2e796a727be
JoanneWang-2 <69273871+JoanneWang-2@users.noreply.github.com>
2021-11-14 20:11:25 -0500

    added more api endpoints

--- Src/server.py
+++ Src/server.py
@@ -1,11 +1,33 @@
-from flask import Flask
-import sys
+from flask import Flask, render_template, request, redirect, jsonify
+from json import dump
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
 app = Flask(__name__)
+
+import os
+import random
+import time
+
+DATABASEURI = os.environ.get("database-uri")
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+#get dining hall menu items
+@app.route('/<hallID>', methods=['POST'])
+def getDiningMenuItems(hallID):
+    pass
+
+#Login Page
+@app.route('/login')
+def login():
+    return "<p>Login here</p>"
+
+#Signup Page
+@app.route('/signup')
+def signup():
+    return "<p>Sign up here</p>"
 
 if __name__ == '__main__':
 
