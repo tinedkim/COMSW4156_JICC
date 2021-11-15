88dff601912be19e5a9d461b3d286d7e3690fb7b
Christine Kim <30416401+tinedkim@users.noreply.github.com>
2021-11-14 20:57:08 -0500

    Fix population and unittest method (#13)
    * Test population
    
    * Test database.py

--- Src/database.py
+++ Src/database.py
@@ -7,20 +7,23 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import *
 from menu_items import scrape_all
+from requests import get
 
-load_dotenv()
+ip = get('https://api.ipify.org').content.decode('utf8')
+# For testing locally:
+ip = '34.74.179.106'
+DATABASEURI = "postgresql://postgres:jicc@{0}/postgres".format(ip)
 
-DATABASEURI = os.environ.get("database-uri")
-
-engine = create_engine(DATABASEURI, connect_args={'connect_timeout': 10})
+engine = create_engine(DATABASEURI)
 
 
 def populate_menu_items():
     menus = scrape_all()
     with engine.connect() as connection:
         for i in range(1, 4):
-            dining_hall = menus[i]
+            dining_hall = menus[i-1]
             for key, val in dining_hall.items():
-                connection.execute("INSERT INTO foodItem(foodName, imageURL, \
-                                   diningHall) VALUES (%s, %s, %d)",
-                                   key, val, dining_hall)
+                connection.execute("INSERT INTO fooditem(foodName, imageURL, diningHall) \
+                                    VALUES (%s, %s, %s) \
+                                    ON CONFLICT (foodName) DO NOTHING", 
+                                    key, val, i)
---
+++ Src/test_database.py
@@ -0,0 +1,31 @@
+from sqlalchemy import *
+import unittest
+import database
+from requests import get
+
+ip = get('https://api.ipify.org').content.decode('utf8')
+# For testing locally:
+ip = '34.74.179.106'
+DATABASEURI = "postgresql://postgres:jicc@{0}/postgres".format(ip)
+
+engine = create_engine(DATABASEURI)
+
+class Test_TestDatabase(unittest.TestCase):
+
+    def test_populate_menu_items(self):
+        database.populate_menu_items()
+
+        # Check if database is not empty
+        with engine.connect() as connection:
+            cursor = connection.execute("SELECT D.id FROM diningHall D")
+            for hall in cursor:
+                id = hall[0]
+                item_counts = connection.execute("SELECT COUNT(*) FROM foodItem F\
+                                            WHERE F.diningHall = {0}".format(id))
+                for count in item_counts:
+                    num_entry = count[0]
+                    print(num_entry, "for id: ", id)
+                    self.assertTrue(num_entry > 0)
+
+if __name__ == '__main__':
+    unittest.main()
