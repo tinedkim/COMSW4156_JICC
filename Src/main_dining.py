51bc3356ff0088ca1266dc3192cf7ede15a724b6
Catelen Wu <catelenwu@gmail.com>
2021-11-15 13:12:49 -0500

    Update style checker files

--- Src/main_dining.py
+++ Src/main_dining.py
@@ -4,20 +4,30 @@ from dotenv import load_dotenv
 import os
 import requests
 
-dining_url = 'https://dining.columbia.edu/'
 
-load_dotenv()
-token = os.environ.get("scraping-token")
-client = ScrapingAntClient(token=token)
+def check_open_halls():
+    dining_url = 'https://dining.columbia.edu/'
 
-content = client.general_request(dining_url).content
-soup = BeautifulSoup(content, features='html5lib')
-open_halls = []
+    load_dotenv()
+    token = os.environ.get("scraping-token")
+    client = ScrapingAntClient(token=token)
 
-dining_halls = soup.find('div', attrs={'class': 'col-xs-12 col-md-6'})
-for hall in dining_halls.findAll('div', attrs={'class': 'location'}):
-    spans = hall.findAll('span')
-    hall_name = spans[0].text
-    hall_status = spans[1].text
-    if hall_status == 'Open':
-        open_halls.append(hall_name)
+    content = client.general_request(dining_url).content
+    soup = BeautifulSoup(content, features='html5lib')
+    print(soup.prettify())
+    open_halls = []
+
+    dining_halls = soup.find('div', attrs={'class': 'col-xs-12 col-md-6'})
+    print(dining_halls)
+    for hall in dining_halls.findAll('div', attrs={'class': 'location'}):
+        spans = hall.findAll('span')
+        hall_name = spans[0].text
+        hall_status = spans[1].text
+        print(hall_status)
+        if hall_status == 'Open' or hall_status == 'Closing Soon':
+            open_halls.append(hall_name)
+    print(open_halls)
+    return open_halls
+
+
+check_open_halls()
