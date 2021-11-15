3d476f8a01c8932f23b7f606985262dd7e7fb656
iChauster <ivan.m.chau@gmail.com>
2021-11-15 21:49:08 +0000

    add api tests

--- Src/test_database.py
+++ Src/test_database.py
@@ -28,8 +28,29 @@ class Test_TestDatabase(unittest.TestCase):
                     print(num_entry, "for id: ", id)
                     self.assertTrue(num_entry > 0)
 
-    def testDiningHallMenuItems(self):
+    def test_dining_hall_menu_items(self):
         res = database.getDiningHallMenuItems(1)
         assert len(res) > 0
+
+    def test_dining_halls(self):
+        res = database.getDiningHalls()
+        assert len(res) > 0
+
+    def test_get_food_reviews(self):
+        res = database.getReviewsForFoodItem(2)
+        assert len(res) > 0
+        res = database.getReviewsForFoodItem(-1)
+        assert len(res) == 0
+
+    def test_get_timestamps_for_dining_hall(self):
+        res = database.getReviewTimestampsForDiningHall(1)
+        assert len(res) > 0
+
+    def test_get_food_items(self):
+        res = database.getFoodItems()
+        assert len(res) > 0
+
+
+
 if __name__ == '__main__':
     unittest.main()
